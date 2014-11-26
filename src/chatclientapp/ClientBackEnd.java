392ed21bb9e69db16ef80cf9f075d27991bb105b
Ohjelmistokehitys <Ohjelmistokehitys@OpiFrame5-THINK>
2014-11-26 13:34:32 +0200

    The first version of chat. Secondary thread created.

---
+++ src/chatclientapp/ClientBackEnd.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatclientapp;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import message.ChatMessage;
+
+/**
+ *
+ * @author Ohjelmistokehitys
+ */
+public class ClientBackEnd implements Runnable{
+
+    private Socket clientSocket;
+    private ObjectOutputStream output;
+    private ObjectInputStream input;
+    
+    //tämä ajetaan UI säikeessä, EDT säikeessä!! MIelellään kaikki toteutus siis run funktioon
+    public ClientBackEnd(){
+        try {
+            clientSocket = new Socket("localhost",3010); //localhost on sama kuin 127.0.0.1
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    //Tämä on sitten omassa säikeessä!!! 
+    //Run funktiota kutsutaan vain kerran
+    @Override
+    public void run() {
+        try {
+            //kun sama objekti kyseessä, näiden järjetyksellä on väliä, ensin kirjoitus, sitten luku?
+            output=new ObjectOutputStream(clientSocket.getOutputStream());
+            input=new ObjectInputStream(clientSocket.getInputStream());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        //read and write from socket until user closes the app
+        while(true){
+            try {
+                ChatMessage m = (ChatMessage)input.readObject();
+                System.out.println(m.getChatMessage());
+            } catch (IOException | ClassNotFoundException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    
+}
